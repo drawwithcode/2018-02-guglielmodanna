a090081b9879dce4e632cd8c9c9db4f600c21da5
Guglielmo D'Anna <hello@guglielmodanna.com>
2018-09-29 22:16:28 +0200

    first commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-function preload(){
-  // put preload code here
-}
+var radius = 220;
+var increment = 1;
+var speed = 1;
 
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+    strokeWeight(2);
+    if (width < radius * 2 || height < radius * 2) {
+        radius = Math.min(width, height)/2 - 10;
+    }
 }
 
 function draw() {
-  // put drawing code here
+    background(0, 3, 10);
+    noFill();
+    noStroke();
+    increment = (mouseX  * 4 / width) +1;
+    speed = (mouseY * 3 / height) +1;
+    for (var i = -90; i < 90; i+= increment) {
+        stroke(175 * sin(i + frameCount * speed), 0, 240 * sin(i - frameCount * speed));
+        line(width/2+radius, height/2,
+            width/2+radius-(radius*2*cos(i)*cos(i)),
+            height/2-(radius*2*cos(i)*sin(i)));
+    }
 }
